f5151aaf523a16b37a59bd9384d40a96530beb6a
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-10 12:01:00 +0530

    Add basic output example - first C++ program

---
+++ Variables & Datatypes/01_basic_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Basic C++ program demonstrating output to console
+int main() {
+    
+    int age = 17;
+    
+    cout << age << endl;
+    cout << "My age is " << age << endl;
+    cout << "Hello, World!" << endl; 
+    
+    return 0;
+}
