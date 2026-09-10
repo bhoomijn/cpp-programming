78b4cea08002ad1f5f82fb1828a9661f6ea5ace5
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-10 11:50:17 +0530

    Update main.cpp

--- Variables & Datatypes/main.cpp
+++ Variables & Datatypes/main.cpp
@@ -26,3 +26,47 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// This is a simple C++ program that demonstrates the use of the C++14 language standard version.
+// int variable declaration and initialization
+// int is a data type that can store whole numbers (both positive and negative).
+
+    int a = 5;
+    cout << a << endl;
+    
+    int n = -10;
+    cout << n << endl;
+
+// float variable declaration and initialization
+// float is a data type that can store decimal numbers (both positive and negative).
+
+    float b = 3.14;
+    cout << b << endl;
+
+    float m = -399.14;
+    cout << m << endl;
+
+// char variable declaration and initialization
+// char is a data type that can store a single character.
+
+    char c = 'A';
+    cout << c << endl;
+
+// bool variable declaration and initialization
+// false is 0, true is 1
+
+    bool d = true;
+    cout << d << endl;
+
+// double is for floating point numbers with double precision
+
+    double e = 3.1488159;
+    cout << e << endl;
+
+    return 0;
+}
