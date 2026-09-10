0784ca4f6568f79e2294dfcff680f221dbd3554d
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-10 11:22:24 +0530

    Add main function to display age and greeting

--- Variables & Datatypes/main.cpp
+++ Variables & Datatypes/main.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 17;
+
+    cout << age << endl;
+    cout << "My age is " << age << endl;
+    cout << "Hello, World!" << endl; 
+
+    return 0;
+}
