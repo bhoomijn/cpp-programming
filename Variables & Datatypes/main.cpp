bfbe301355eb85184591cfd240204826ec7c2b22
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-10 11:38:19 +0530

    Update main.cpp

--- Variables & Datatypes/main.cpp
+++ Variables & Datatypes/main.cpp
@@ -12,3 +12,17 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 17;
+    cout << age << endl;
+    
+    int age = 66;
+    cout << age << endl; 
+
+    return 0;
+}
