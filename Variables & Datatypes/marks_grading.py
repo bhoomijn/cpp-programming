bfc10efe34385169324b244f81d69de3401e41ff
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-12 17:04:17 +0530

    Update marks_grading.py

--- Variables & Datatypes/marks_grading.py
+++ Variables & Datatypes/marks_grading.py
@@ -1,1 +1,15 @@
 
+# marks_grading.py
+
+marks = int(input("Enter your marks: "))
+
+if marks > 90:
+    print("A")
+elif 81 <= marks <= 90:
+    print("B")
+elif 71 <= marks <= 80:
+    print("C")
+elif 61 <= marks <= 70:
+    print("D")
+else:
+    print("Fail")
