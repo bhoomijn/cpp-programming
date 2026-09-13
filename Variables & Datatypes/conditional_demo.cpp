55ad3e63e85b45879b5d5b227af0eb764dbf16b7
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-13 23:15:24 +0530

    Update conditional_demo.cpp

--- Variables & Datatypes/conditional_demo.cpp
+++ Variables & Datatypes/conditional_demo.cpp
@@ -1,1 +1,22 @@
 
+// marks_grading.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cout << "Enter marks" << endl;
+    cin >> marks;
+
+    if (marks >= 90) {
+        cout << "A" << endl;
+    } else if (marks >= 80) {
+        cout << "B" << endl;
+    } else if (marks >= 70) {
+        cout << "C" << endl;
+    } else {
+        cout << "Fail" << endl;
+    }
+
+    return 0;
+}
