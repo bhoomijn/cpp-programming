34302d3fbea70162ff2303e1da31aaf6ead6a5f5
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-14 00:50:53 +0530

    Update ternary_max.cpp

--- Variables & Datatypes/ternary_max.cpp
+++ Variables & Datatypes/ternary_max.cpp
@@ -1,1 +1,14 @@
 
+// max_of_two.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 8;
+
+    int result = (x > y) ? x : y;
+    cout << result << endl;
+
+    return 0;
+}
