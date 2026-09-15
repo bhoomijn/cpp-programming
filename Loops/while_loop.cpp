16b05654b87e58891c0846de67f87f88837a571f
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-15 10:51:11 +0530

    Update while_loop.cpp

--- Loops/while_loop.cpp
+++ Loops/while_loop.cpp
@@ -1,1 +1,13 @@
 
+// while loop demo
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 5) {
+        cout << i << endl;
+        i = i + 1;
+    }
+    return 0;
+}
