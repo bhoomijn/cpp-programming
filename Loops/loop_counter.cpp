c8240fbab666b9e042a6c1426ac8c9285a85acc7
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-15 10:05:05 +0530

    Update loop_counter.cpp

--- Loops/loop_counter.cpp
+++ Loops/loop_counter.cpp
@@ -1,1 +1,10 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int count = 1; count <= 2; count = count + 1) {
+        cout << count << endl;  // prints 1 then 2
+    }
+    return 0;
+}
