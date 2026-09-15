1499aa6065b82f103908b80707e6041bdf1aed47
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-15 11:55:33 +0530

    Update do_while_loop.cpp

--- Loops/do_while_loop.cpp
+++ Loops/do_while_loop.cpp
@@ -1,1 +1,17 @@
 
+// do-while loop demo
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 8;
+    int i = 1;
+    do {
+        cout << "bhoomi" << endl;  
+        i = i + 1;      
+    } while (i <= a);
+
+    return 0;
+}
+
