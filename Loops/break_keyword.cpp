1abbada8165aeb5c201edfd05a133c22b6d8827e
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-15 10:20:23 +0530

    Update break_keyword.cpp

--- Loops/break_keyword.cpp
+++ Loops/break_keyword.cpp
@@ -1,1 +1,14 @@
 
+// Demonstration of break keyword
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << i << endl;
+        if (i == 5) {
+            break;  // exits loop when i = 5
+        }
+    }
+    return 0;
+}
