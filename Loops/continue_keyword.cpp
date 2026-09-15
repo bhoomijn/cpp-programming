c9d7e6b5f5a98aa3bc7a1c478b0f4453e89c29e7
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-15 10:37:08 +0530

    Update continue_keyword.cpp

--- Loops/continue_keyword.cpp
+++ Loops/continue_keyword.cpp
@@ -1,1 +1,14 @@
 
+// continue keyword demo
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            continue;  // skips printing when i = 5
+        }
+        cout << i << endl;
+    }
+    return 0;
+}
