ca947574836b672a0e4148470c86867571043236
Bhoomi Jain <bhoomijn4@gmail.com>
2026-09-21 00:38:06 +0530

    Create arithmetic_operators.cpp

---
+++ Operators/arithmetic_operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 5;
+
+    cout << "a + b = " << a + b << endl;   // Addition
+    cout << "a * b = " << a * b << endl;   // Multiplication
+    cout << "a / b = " << a / b << endl;   // Division
+    cout << "a % b = " << a % b << endl;   // Modulus (remainder)
+
+    return 0;
+}
